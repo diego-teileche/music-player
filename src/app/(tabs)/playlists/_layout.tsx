47d0a51503293ad1fc8654550902694788f255f9
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-21 19:46:33 -0300

    Layouts header styles added

--- src/app/(tabs)/playlists/_layout.tsx
+++ src/app/(tabs)/playlists/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from 'expo-router'
 import { View } from 'react-native'
 import { defaultStyles } from '@/styles'
+import { StackScreenWithSearchBar } from '@/constants/layout'
 
 const PlaylistsScreenLayout = () => {
 	return (
 		<View style={defaultStyles.container}>
 			<Stack>
-				<Stack.Screen name="index" options={{ headerTitle: 'Playlists' }} />
+				<Stack.Screen
+					name="index"
+					options={{ ...StackScreenWithSearchBar, headerTitle: 'Playlists' }}
+				/>
 			</Stack>
 		</View>
 	)
